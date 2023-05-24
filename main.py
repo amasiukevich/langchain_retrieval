313e656a909f149543d327998b9c74e2a97b0f35
amasiukevich <anton.masiukiewicz1@gmail.com>
2023-05-24 18:12:58 +0200

    Implemented frontend truly

---
+++ main.py
@@ -0,0 +1,62 @@
+# this file will be holding streamlit frontend
+
+from backend.core import run_llm
+from typing import Set
+import streamlit as st
+
+from streamlit_chat import message
+
+st.header("Langchain Doc Helper Bot")
+
+prompt = st.text_input("Prompt", placeholder="Enter your prompt here...")
+
+
+# Session state stuff
+if "user_prompt_history" not in st.session_state:
+    st.session_state["user_prompt_history"] = []
+
+if "chat_answers_history" not in st.session_state:
+    st.session_state["chat_answers_history"] = []
+
+
+def create_sources_string(source_urls: Set[str]) -> str:
+    if not source_urls:
+        return ""
+
+    sources_list = list(source_urls)
+
+    sources_string = "sources:\n"
+    for i, source in enumerate(sources_list):
+        sources_string += f"{i + 1}. {source}\n"
+
+    return sources_string
+
+
+if prompt:
+    with st.spinner("Generating response..."):
+        import time
+
+        time.sleep(3)
+
+        generated_response = run_llm(query=prompt)
+        sources = set(
+            [doc.metadata["source"] for doc in generated_response["source_documents"]]
+        )
+
+        print("Generating from LLM")
+        formatted_response = (
+            f"{generated_response['result']} \n\n {create_sources_string(sources)}"
+        )
+
+        print("Hey there")
+
+        st.session_state["user_prompt_history"].append(prompt)
+        st.session_state["chat_answers_history"].append(formatted_response)
+
+
+if st.session_state["chat_answers_history"]:
+    for user_query, generated_response in zip(
+        st.session_state["user_prompt_history"], st.session_state["chat_answers_history"]
+    ):
+        message(user_query, is_user=True)
+        message(generated_response, is_user=False)
